1d1759e40d4b6d970146bec3cafed97a84e747ee
antoine-bibb <ajbibb0114@gmail.com>
2020-11-11 17:09:12 -0500

    finsihed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./component/Form";
+import styled from "styled-components";
+import "./App.css";
+
+//Styled components
+const AppContainer = styled.div`
+  width: 80%;
+  margin: auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Form />
+    </AppContainer>
   );
 }
 
